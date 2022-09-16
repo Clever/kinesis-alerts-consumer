d37aedb4db1cc1e7ea4e3de2c8a3567fca69be16
Andrew Marine <andrew.marine@clever.com>
2022-09-16 16:10:05 -0700

    link so article

--- delay.go
+++ delay.go
@@ -8,7 +8,10 @@ import (
 // Generally you want to steer clear of sync/atomic, but simple numbers like this where
 // you are concurrently storing a usage statistic is one of the few cases where it
 // makes sense over a mutex. atomic does not provide read/write ordering guarantees
-// which don't matter here as much as general trends do.
+// which don't matter here as much as general trends do. Be very careful about
+// any changes, as there are a lot of hidden gotchas when working with atomic values.
+// Read in detail the discussion here:
+// https://stackoverflow.com/questions/47445344/is-there-a-difference-in-go-between-a-counter-using-atomic-operations-and-one-us
 var maxDelay int64 = 0
 
 func updateMaxDelay(ts []time.Time) {
